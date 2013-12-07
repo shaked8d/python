1a039d3b787800c1bf6a0cdc596c1b7788d42c21
shaked8d <shaked8d@gmail.com>
2013-12-07 13:43:34 +0200

    Finished Milon Kiss include tries

--- MilonKiss.py
+++ MilonKiss.py
@@ -2,20 +2,24 @@
 
 def AddWord(dict, eng_word, heb_word):
     dict[eng_word] = heb_word
-    return "The word " + eng_word + "added with the trans" + heb_word
+    return dict
 
 def SearchWord(dict, eng_word):
     if dict.has_key(eng_word) == True:
-        return dict[eng_word]
+        print "The translation of " + eng_word + " is " + dict[eng_word]
+        return True
     else:
-        return "I don't have the word " + eng_word + " in me :("
+        print "I don't have the word " + eng_word + " in me :("
+        return False
 
 def DeleteWord(dict, eng_word):
     answer = SearchWord(dict, eng_word)
-    if answer == dict[eng_word]:
+    if answer == True:
         del dict[eng_word]
+        print "DELETED \n"
+        return dict
     else:
-        return "I don't have the word " + eng_word + "in me :("
+        return dict
 
 def Exit():
     return 0
@@ -31,25 +35,30 @@ def GetUserHebWord():
 dict = {}
 user = 0
 while user != 4:
-    user = raw_input("""Welcome! Enter what would you like to do:
+    user = raw_input("""\n Welcome! Enter what would you like to do:
         1) add a word
         2) search a word
         3) delete a word
-        4) Exit the program
-        
-        """)
-    print "You entered ", user + " "
+        4) Exit the program\n""")
+    print "You entered ", user + " \n"
+
     if user == '1':
         eng_word = GetUserEngWord()
         heb_word = GetUserHebWord()
-        AddWord(dict, eng_word, heb_word)
+        dict = AddWord(dict, eng_word, heb_word)
+        print dict
+
     elif user == '2':
         eng_word = GetUserEngWord()
         SearchWord(dict, eng_word)
+
     elif user == '3':
         eng_word = GetUserEngWord()
-        DeleteWord(doct, eng_word)
+        dict = DeleteWord(dict, eng_word)
+
     elif user == '4':
+        print "Bye Bye :) \n"
         exit()
+
     else:
-        print "You entered a wrong number. Type again"
+        print "You entered a wrong number. Type again \n"
